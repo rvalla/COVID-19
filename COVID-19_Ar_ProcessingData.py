45757e9116930b21134044efd7b9ae829c57ea7c
Rodrigo Valla <rodrigovalla@protonmail.ch>
2020-08-06 16:18:43 -0300

    Updating data and charts...

--- COVID-19_Ar_ProcessingData.py
+++ COVID-19_Ar_ProcessingData.py
@@ -26,7 +26,7 @@ fileName = "Argentina.csv"
 fileCompletePath = "Argentina_Data/" + fileName
 
 dataStartDate = "2020-03-03"
-dataEndDate = "2020-08-02"
+dataEndDate = "2020-08-05"
 dataPeriod = pd.date_range(dataStartDate, dataEndDate)
 
 datapath = "Argentina_Data/processed_data/"
--- COVID-19_DataGobAr_TestsAnalysis.py
+++ COVID-19_DataGobAr_TestsAnalysis.py
@@ -24,14 +24,14 @@ chartPath = "Argentina_Data/actual_charts/"
 colorlist = ["orange", "tab:blue", "tab:red", "tab:green"]
 
 dataStartDate = "2020-02-11"
-dataEndDate = "2020-07-29"
+dataEndDate = "2020-08-05"
 wantedStartDate = "2020-04-15"
-wantedEndDate = "2020-07-28"
+wantedEndDate = "2020-08-03"
 dataPeriod = pd.date_range(dataStartDate, dataEndDate)
 plotScale = "linear"
 
 #Deciding language for titles and tags...
-lg = 0 # 0 for english, 1 for spanish
+lg = 1 # 0 for english, 1 for spanish
 
 #Deciding if you want to save and show charts...
 saveChart = True
@@ -53,7 +53,7 @@ del dataframe["codigo_indec_provincia"]
 del dataframe["codigo_indec_departamento"]
 del dataframe["localidad"]
 del dataframe["codigo_indec_localidad"]
-del dataframe["origen_de_financiamiento"]
+del dataframe["origen_financiamiento"]
 del dataframe["tipo"]
 del dataframe["ultima_actualizacion"]
 
