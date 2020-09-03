45cfcf02980decf3395c26b76f81d7d65e67789b
Rodrigo Valla <rodrigovalla@protonmail.ch>
2020-09-03 17:45:24 -0300

    Updating data and charts...

--- COVID-19_Ar_ProcessingData.py
+++ COVID-19_Ar_ProcessingData.py
@@ -26,7 +26,7 @@ fileName = "Argentina.csv"
 fileCompletePath = "Argentina_Data/" + fileName
 
 dataStartDate = "2020-03-03"
-dataEndDate = "2020-08-31"
+dataEndDate = "2020-09-02"
 dataPeriod = pd.date_range(dataStartDate, dataEndDate)
 
 datapath = "Argentina_Data/processed_data/"
